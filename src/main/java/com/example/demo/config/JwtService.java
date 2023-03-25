05469971f631a0dac0d4b7c0e0c244d8921a6ff0
Neelkanth Singh <neelkanthsingh.jr@gmail.com>
2023-03-25 22:57:02 +0530

    Created a JWT parse builder which verifies the signature(signing algo + scret key)

--- src/main/java/com/example/demo/config/JwtService.java
+++ src/main/java/com/example/demo/config/JwtService.java
@@ -1,10 +1,33 @@
 package com.example.demo.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "5A7234753778214125442A472D4B6150645367566B58703273357638792F423F";
     public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keysBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keysBytes);
     }
 }
