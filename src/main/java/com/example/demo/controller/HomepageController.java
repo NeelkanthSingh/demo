455ad6689b4347cd5dcf1545748429dddb378f34
Neelkanth Singh <neelkanthsingh.jr@gmail.com>
2023-03-27 20:42:17 +0530

    Additional endpoint to verify that authentication is indeed working

--- src/main/java/com/example/demo/controller/HomepageController.java
+++ src/main/java/com/example/demo/controller/HomepageController.java
@@ -1,41 +1,17 @@
 package com.example.demo.controller;
 
-import com.example.demo.models.request.UserDetailsRequest;
-import com.example.demo.models.response.UserDetailsResponse;
-import com.example.demo.service.HomepageService;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 // Controller class to store the user details
-@Controller
-@RequestMapping("")
+@RestController
+@RequestMapping("/demo")
 public class HomepageController {
 
-    @Autowired
-    public HomepageService homepageService;
-
-    // Create a logger instance
-    Logger logger = LoggerFactory.getLogger(HomepageController.class);
-
-    // Method to store the user details
-    @PostMapping("/homepage")
-    ResponseEntity<String> storeUserDetails(@RequestBody(required = true) @NotNull UserDetailsRequest userDetailsRequest) {
-        logger.info("This is the User Details: " + userDetailsRequest.toString());
-        return homepageService.saveUserDetails(userDetailsRequest);
-    }
-
     @GetMapping("/homepage")
-    ResponseEntity<UserDetailsResponse> getUserDetails(@RequestParam @NotNull String identifier){
-        logger.info("This is the Identifier: " + identifier);
-        return homepageService.getUserDetails(identifier);
+    ResponseEntity<String> getHome(){
+        return ResponseEntity.ok("You have reached homepage.");
     }
-
-
 
 }
-
