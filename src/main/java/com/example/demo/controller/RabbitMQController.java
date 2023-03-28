142d100a9fd8f1d397cc267d1d76c7bf51596533
Neelkanth Singh <neelkanthsingh.jr@gmail.com>
2023-03-28 23:09:20 +0530

    Publish code added

--- src/main/java/com/example/demo/controller/RabbitMQController.java
+++ src/main/java/com/example/demo/controller/RabbitMQController.java
@@ -1,25 +1,29 @@
 package com.example.demo.controller;
 
-import com.example.demo.rabbitmq.Publisher;
+import com.example.demo.model.MessageObject;
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 @Controller
-@RequestMapping("/pub")
+@RequestMapping("/send")
 @RequiredArgsConstructor
 public class RabbitMQController {
 
-    private final Publisher publisher;
+    private final RabbitTemplate rabbitTemplate;
 
-    @GetMapping("/post")
-    ResponseEntity<String> publishMessage() throws IOException, TimeoutException {
-        publisher.publish();
-        return ResponseEntity.ok("First Message Sent");
+    @GetMapping("/message")
+    ResponseEntity<String> publishMessage(@RequestParam String message) throws IOException, TimeoutException {
+        MessageObject messageObject = MessageObject.builder().id(1).message(message).build();
+        rabbitTemplate.convertAndSend("Mobile", messageObject);
+        return ResponseEntity.ok("Message Sent");
     }
 }
