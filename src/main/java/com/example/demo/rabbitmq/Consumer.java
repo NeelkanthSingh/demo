e727cfda9b7b3e029e891a05adc0fbded96a8059
Neelkanth Singh <neelkanthsingh.jr@gmail.com>
2023-03-28 23:48:45 +0530

    Consumer code added

---
+++ src/main/java/com/example/demo/rabbitmq/Consumer.java
@@ -0,0 +1,15 @@
+package com.example.demo.rabbitmq;
+
+import com.example.demo.model.MessageObject;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Consumer {
+
+    @RabbitListener(queues = "Mobile")
+    public void getMessage(MessageObject message){
+        System.out.println(message.getMessage());
+    }
+}
