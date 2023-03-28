05626e8fa34669fe98766eea8aeb8d2baba1c9e9
Neelkanth Singh <neelkanthsingh.jr@gmail.com>
2023-03-28 22:37:51 +0530

    Added code for props in case of Header-Exchange. **NOTE: THIS IS NOT WORKING**

--- src/main/java/com/example/demo/rabbitmq/Publisher.java
+++ src/main/java/com/example/demo/rabbitmq/Publisher.java
@@ -1,12 +1,13 @@
 package com.example.demo.rabbitmq;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.TimeoutException;
 
 @Component
@@ -20,6 +21,16 @@ public class Publisher {
         Channel channel = connection.createChannel();
 
         String message = "First message";
+
+        // Below is the props type, generally used in case of Headers-Exchange
+
+        /**
+        Map<String, Object> headers = new HashMap<String, Object>();
+        headers.put("item1", "Mobile");
+        headers.put("item2", "Television");
+
+        BasicProperties basicProperties = new AMQP.BasicProperties.Builder().headers(headers).build();
+        **/
 
         channel.basicPublish("", "Queue-1", null, message.getBytes());
 
