94def02325237f4f26df1bc0929096f6592a0efc
Neelkanth Singh <neelkanthsingh.jr@gmail.com>
2023-04-06 12:57:03 +0530

    Made a little correction in the code to get it working

--- src/main/java/com/example/demo/config/AutoCreateConfig.java
+++ src/main/java/com/example/demo/config/AutoCreateConfig.java
@@ -17,21 +17,4 @@ public class AutoCreateConfig {
                 .build();
     }
 
-    @Bean
-    public NewTopic libraryEventsRetry() {
-
-        return TopicBuilder.name("${topics.retry}")
-                .partitions(3)
-                .replicas(3)
-                .build();
-    }
-
-    @Bean
-    public NewTopic libraryEventsDlt() {
-
-        return TopicBuilder.name("${topics.dlt}")
-                .partitions(3)
-                .replicas(3)
-                .build();
-    }
 }
