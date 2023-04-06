145f3651aca153bf9cb01e23fa63bf1e3e37cb6e
Neelkanth Singh <neelkanthsingh.jr@gmail.com>
2023-04-06 11:47:00 +0530

    Adedd code for CommonErrorHandler which also perform retries

--- src/main/java/com/example/demo/config/LibraryEventsConsumerConfig.java
+++ src/main/java/com/example/demo/config/LibraryEventsConsumerConfig.java
@@ -7,10 +7,17 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
 @EnableKafka
 public class LibraryEventsConsumerConfig {
+
+    public DefaultErrorHandler errorHandler() {
+        var fixedBackOff = new FixedBackOff(1000L, 2);
+        return new DefaultErrorHandler(fixedBackOff);
+    }
 
     @Bean
     ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
@@ -19,6 +26,7 @@ public class LibraryEventsConsumerConfig {
         ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         configurer.configure(factory, kafkaConsumerFactory);
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        factory.setCommonErrorHandler(errorHandler()); // This is to handle the error in the consumer. With this, we can retry the message, however many times.
         // factory.setConcurrency(3); // This is to spin up multiple threads to consume, use only if you are not running on cloud.
         return factory;
 
